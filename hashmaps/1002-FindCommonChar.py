d869742f638e43fc4087e26a62896d8b4c22eb77
Taran kalirao <kaliraotaran@email.com>
2024-03-01 00:33:17 +0530

    renamed:    1-2sums.py -> blind75/1-2sums.py 	renamed:    104-MaxDepthofBTree.py -> blind75/104-MaxDepthofBTree.py 	renamed:    110-balancedBinaryTree.py -> blind75/110-balancedBinaryTree.py 	renamed:    121-buysellStocks.py -> blind75/121-buysellStocks.py 	renamed:    125-validPalendrome.py -> blind75/125-validPalendrome.py 	renamed:    141-LinkedListCycle.py -> blind75/141-LinkedListCycle.py 	renamed:    15-3sums.py -> blind75/15-3sums.py 	renamed:    167-2sumsll.py -> blind75/167-2sumsll.py 	renamed:    169-MajorityElement.py -> blind75/169-MajorityElement.py 	renamed:    198-RobHouse.py -> blind75/198-RobHouse.py 	renamed:    20-validParenthesis.py -> blind75/20-validParenthesis.py 	renamed:    206-reverseLinkedlist.py -> blind75/206-reverseLinkedlist.py 	renamed:    21-merge2sortedlist.py -> blind75/21-merge2sortedlist.py 	renamed:    217-ContainDuplicate.py -> blind75/217-ContainDuplicate.py 	renamed:    226-InvertBinaryTree.py -> blind75/226-InvertBinaryTree.py 	renamed:    232-queueUsingStack.py -> blind75/232-queueUsingStack.py 	renamed:    235-LowestComAncestor.py -> blind75/235-LowestComAncestor.py 	renamed:    242-Anagram.py -> blind75/242-Anagram.py 	renamed:    278-firstbsdversion.py -> blind75/278-firstbsdversion.py 	renamed:    383-ransomNote.py -> blind75/383-ransomNote.py 	renamed:    409-longestPalindrome.py -> blind75/409-longestPalindrome.py 	renamed:    41-productArrExceptSelf.py -> blind75/41-productArrExceptSelf.py 	renamed:    462-FloodFill.py -> blind75/462-FloodFill.py 	renamed:    49-groupAnagram.py -> blind75/49-groupAnagram.py 	renamed:    53-maxsubarray.py -> blind75/53-maxsubarray.py 	renamed:    67-addBinary.py -> blind75/67-addBinary.py 	renamed:    690-RansomNote.py -> blind75/690-RansomNote.py 	renamed:    70-climbingstairs.py -> blind75/70-climbingstairs.py 	renamed:    704-BinarySearch.py -> blind75/704-BinarySearch.py 	renamed:    876-MiddlelinkedList.py -> blind75/876-MiddlelinkedList.py 	new file:   hashmaps/1002-FindCommonChar.py

---
+++ hashmaps/1002-FindCommonChar.py
@@ -0,0 +1,40 @@
+'''
+https://leetcode.com/problems/find-common-characters/submissions/1189958063/
+
+we have to return common character in all the different words from the list
+
+we first add the character of the first word to a hashmap
+
+then in a seperate for loop (it loops from the second word to the last)
+    we create a new seperate hashmap
+    we then iterate through each character of the a word
+        if the character was already in the second hashmap and if the number of characters in 
+                original hash is less than the num. of characters in teh original hash:
+            we then incremnt the value of that character in the hash by 1
+    in end of inner loop, we assign the value of new hash to the original hash
+
+    
+
+
+'''
+
+class Solution:
+    def commonChars(self, words: list[str]) -> list[str]:
+        hash1 = defaultdict(int)
+        for char in words[0]:
+            hash1[char] += 1
+        
+        for i in range(1, len(words)):
+            new_hash = defaultdict(int)
+            for char in words[i]:
+                if char in hash1 and new_hash[char] < hash1[char]:
+                    new_hash[char] += 1
+            hash1 = new_hash
+
+        ret = []
+        for k in new_hash:
+            for _ in range(new_hash[k]):
+                ret.append(k)
+        
+        return ret
+
