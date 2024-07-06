481369c28bcf647c20f970b8daa2b04672eae8d3
Taran kalirao <kaliraotaran@gmail.com>
2024-07-07 00:32:36 +0530

    modified:   array/3-searchInsertPosition.py 	modified:   array/7-SingleNumber.py

--- array/3-searchInsertPosition.py
+++ array/3-searchInsertPosition.py
@@ -13,3 +13,16 @@ class Solution:
                 return i
         return len(nums) # this means that the element belongs at the end since its the largest
                             # because it did not belong in the for loop above(wasn't in the array)
+        
+        #  l =0 
+        # r = len(nums)-1
+        # mid =0 
+        # while l<=r:
+        #     mid = (l+r)//2
+        #     if nums[mid]<target:
+        #         l = mid+1
+        #     elif nums[mid]>target:
+        #         r = mid-1
+        #     else:
+        #         return mid
+        # return l
