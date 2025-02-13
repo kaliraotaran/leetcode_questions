4f0aca7a9a1c746884fdffffbbca8af1341d3602
Taran kalirao <kaliraotaran@gmail.com>
2025-02-13 10:56:03 +0530

    modified:   array/10-missingNumber.py

--- array/10-missingNumber.py
+++ array/10-missingNumber.py
@@ -9,8 +9,14 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
         
-        summ = sum(nums)
-        c = 0
-        for i in range( len(nums)+1):
-            c+=i
-        return c - summ
+        # summ = sum(nums)
+        # c = 0
+        # for i in range( len(nums)+1):
+        #     c+=i
+        # return c - summ
+
+        # ORR
+        for i in range(len(nums)):
+            if i != nums[i]:
+                return i
+        return len(nums)
